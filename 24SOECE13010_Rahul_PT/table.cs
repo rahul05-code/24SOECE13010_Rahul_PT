736076948f6d886ba978421ab0448f0cb01b75e6
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-10 10:28:33 +0530

    t1

---
+++ 24SOECE13010_Rahul_PT/table.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_Rahul_PT
+{
+    internal class table
+    {
+        static void Main(string[] args)
+        {
+            int num, res, i;
+            Console.WriteLine("Enter number :");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            i = 1;
+            while (i <= 10)
+            {
+                res = num * i;
+                Console.WriteLine("{0} * {2} = {1}",num,res,i);
+                i++;
+
+            }
+        }
+    }
+}
