65f02bc1499886c3f40762121a5ddea800e88f7f
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-10 10:03:33 +0530

    Add project files.

---
+++ 24SOECE13010_Rahul_PT/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_Rahul_PT
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 5; i++)
+            {
+                int rows = 5;
+                for (int j = rows; j >= i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 24SOECE13010_Rahul_PT/pattern.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_Rahul_PT
+{
+    internal class pattern
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                int rows = 5;
+                for (int j = 1; j <= rows; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
