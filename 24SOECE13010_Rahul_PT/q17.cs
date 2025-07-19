757d0883fde069ecd3070557ba1ea61a0b172db9
rahul05-code <kanzariyarahul31@gmail.com>
2025-07-19 17:56:38 +0530

    test

---
+++ 24SOECE13010_Rahul_PT/q17.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_Rahul_PT
+{
+    internal class q17
+    {
+        public static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j <5; j++)
+                {
+                    Console.Write(i);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
