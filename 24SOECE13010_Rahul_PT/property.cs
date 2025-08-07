8ffbc9ddca7143d45de417213a8d23a4758b831e
rahul05-code <kanzariyarahul31@gmail.com>
2025-08-07 10:44:07 +0530

    property

---
+++ 24SOECE13010_Rahul_PT/property.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13010_Rahul_PT
+{
+    class student
+    {
+        private string fname;
+        private string lname;
+
+        //
+        public string getName()// with out property
+        {
+            return fname;
+        }
+        public string setLname(string fname)//without property
+        {
+            return fname;
+        }
+        //
+
+        public string FirstName//with property
+        {
+            get {  return lname; }
+            set { lname = value; }
+        }
+    }
+    internal class property
+    {
+        public static void Main(string[] args)
+        {
+            student s1 = new student();
+
+            s1.setLname("RK");// without property
+
+            s1.FirstName = "RK"; // this is using property
+        }
+    }
+}
